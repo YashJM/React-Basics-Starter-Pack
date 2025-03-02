9a41b9025b8ac9dd74b18dc77ddb613b1479ea54
YashJM <mistryyash100@gmail.com>
2025-03-02 17:09:47 -0500

    📝 Implemented update todo item

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -11,30 +11,62 @@ const Todo: React.FC = () => {
   const [input, setInput] = useState<string>('');
   const [todos, setTodos] = useState<todo[]>([]);
 
-  const addToDo = (e: React.FormEvent<HTMLFormElement>) => {
+  const [editingMode, setEditingMode] = useState<boolean>(false);
+  const [editingId, setEditingId] = useState<number>(0);
+
+  const saveTodo = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+
+    if (editingMode) {
+      const newTodos = todos.map((todo) => {
+        if (todo.id === editingId) {
+          return { ...todo, label: input };
+        }
+        return todo;
+      });
+
+      setTodos(newTodos);
+      resetForm();
+      return;
+    }
+
     setTodos([
       ...todos,
       { id: todos.length, label: input, pinned: false, done: false },
     ]);
+
+    resetForm();
+  };
+
+  const resetForm = () => {
+    setInput('');
+    setEditingMode(false);
   };
 
   const deleteToDo = (id: number) => {
     setTodos(todos.filter((todo) => todo.id !== id));
   };
 
+  const onEdit = (id: number) => {
+    setEditingId(id);
+    setInput(todos.find((todo) => todo.id === id)!.label);
+    setEditingMode(true);
+  };
+
   return (
     <div>
-      <form onSubmit={addToDo}>
+      <form onSubmit={saveTodo}>
         <input value={input} onChange={(e) => setInput(e.target.value)} />
-        <button type='submit'>Add</button>
+        <button type='submit'>{editingMode ? 'Save' : 'Add'}</button>
       </form>
       <ul>
         {todos.map((todo) => (
           <>
             <li key={todo.id}>
+              <button onClick={() => onEdit(todo.id)}>📝</button>
+              &nbsp;
               {todo.label}&nbsp;&nbsp;
-              <button onClick={() => deleteToDo(todo.id)}> X</button>
+              <button onClick={() => deleteToDo(todo.id)}>X</button>
             </li>
           </>
         ))}
